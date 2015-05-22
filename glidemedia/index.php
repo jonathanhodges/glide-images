5cd0a1196b9c5c4bca4beab652dfe45a692fd8bb
cnanninga <chris.nanninga@classyllama.com>
2015-05-21 20:34:31 -0500

    Primary Glide image functionality

--- glidemedia/index.php
+++ glidemedia/index.php
@@ -1,2 +1,16 @@
 <?php
-echo 'Test';
+
+require_once('glide-config.php');
+
+require_once(CLS_GLIDEIMAGES_COMPOSER_VENDOR_DIR);
+
+$uri = $_SERVER['REQUEST_URI'];
+$path = preg_replace('#^/?glidemedia/#', '', $uri);
+$path = preg_replace('#\?.*$#', '', $path);
+
+$server = League\Glide\ServerFactory::create([
+    'source' => CLS_GLIDEIMAGES_SOURCE_DIR,
+    'cache' => 'cache',
+]);
+
+$server->outputImage($path, $_GET);
